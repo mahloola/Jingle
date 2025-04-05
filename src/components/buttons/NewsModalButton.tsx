15a5099f1450bc7474d4d464de872318c0193041
mahloola <mahloola08@gmail.com>
2025-04-05 04:39:55 -0400

    added news post for apr 5

--- src/components/buttons/NewsModalButton.tsx
+++ src/components/buttons/NewsModalButton.tsx
@@ -24,17 +24,69 @@ export default function NewsModalButton({
         open={open}
         onClose={onClose}
       >
-        <p>Some updates 😃</p>
-        <ul>
-          <li>Varlamore added</li>
-          <li>
-            Preference settings: region selection, 2004 audio, hard mode, and a
-            confirmation button (no more misclicks!)
-          </li>
-          <li>Stats (song success rates, your best streak, etc.)</li>
-        </ul>
-
-        <h6>Apr 4, 2025</h6>
+        <img
+          className='modal-bg-image'
+          src='https://storage.googleapis.com/jingle-media/newspaper.png'
+        ></img>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            width: '100%',
+          }}
+        >
+          <h2>News</h2>
+          <h6 className='news-date'>Apr 5, 2025</h6>
+        </div>
+        <p>
+          - Varlamore added
+          <br />- Stats (success%, max streak, etc.)
+          <br />- Preference settings:
+          <br />
+          &emsp;- Region selection
+          <br />
+          &emsp;- 2004 audio
+          <br />
+          &emsp;- Hard mode
+          <br />
+          &emsp;- Confirmation button (no more misclicks)
+        </p>
+        <p>
+          Hi guys, this is mahloola.
+          <br />
+          Thank you for playing Jingle! We notice and appreciate you all. Big
+          thanks to{' '}
+          <a
+            href=''
+            className='link'
+          >
+            FunOrange
+          </a>
+          ,{' '}
+          <a
+            href=''
+            className='link'
+          >
+            Kunito Moe
+          </a>
+          , and the{' '}
+          <a
+            href=''
+            className='link'
+          >
+            wiki map editors
+          </a>{' '}
+          for helping me with these updates!
+          <br />
+          Enjoy these new features, and feel free to join the{' '}
+          <a
+            href='https://discord.gg/7sB8fyUS9W'
+            className='link'
+          >
+            discord! 😊
+          </a>
+        </p>
       </Modal>
     </>
   );
