8b11e6c25d82587a4ed9bfa050ae22f84cd817c5
Jun Villa <jun@aucto.com>
2025-04-12 12:33:03 -0400

    refactor: write function to do click geometry

--- src/utils/map-utils.ts
+++ src/utils/map-utils.ts
@@ -1,6 +1,10 @@
 import { Feature, Polygon, Position } from 'geojson';
 import { Line, Point } from '../types/geometry';
 import { decodeHTML } from './string-utils';
+import geojsondata from '../data/GeoJSON';
+import L from 'leaflet';
+import { equals } from 'ramda';
+import { booleanContains, booleanPointInPolygon, polygon } from '@turf/turf';
 
 const scaleFactor = 3;
 // Ours refers to the pixel coordinates of the map grid we're using
@@ -92,3 +96,142 @@ export const isFeatureVisibleOnMap = (feature: Feature<Polygon>) =>
       return y > 0;
     }),
   );
+
+export const findNearestPolygonWhereSongPlays = (
+  map: L.Map,
+  song: string,
+  leaflet_ll_click: L.LatLng,
+): {
+  polygon: Feature<Polygon>;
+  clicked: boolean;
+  distance: number;
+} => {
+  // prefixes represent 3 coordinate systems:
+  // 1. geojson_xy_* geojson coordinates [x, y]
+  //   - song->location data (/data/GeoJSON.ts)
+  // 2. leaflet_xy_* leaflet coordinates [x, y]
+  //   - used as an intermediary between the two
+  //   - used for calculations
+  // 3. leaflet_ll_* leaflet coordinates { lat, lng }
+  //   - received as input in onClick event
+  //   - used to display points and polygons on the map
+  // use converstion functions to convert between the two
+
+  const leaflet_xy_click = leaflet_ll_to_leaflet_xy(map, leaflet_ll_click);
+  const correctFeature = geojsondata.features.find(featureMatchesSong(song))!;
+  const geojson_xy_correctPolygons = correctFeature.geometry.coordinates;
+  const geojson_xy_repairedPolygons = geojson_xy_correctPolygons.map(
+    closePolygon,
+  ) as Point[][];
+
+  const geojson_xy_nearestPolygon = geojson_xy_repairedPolygons.sort(
+    (polygon1: Point[], polygon2: Point[]) => {
+      const c1 = getCenterOfPolygon(polygon1.map(geojson_xy_to_leaflet_xy));
+      const c2 = getCenterOfPolygon(polygon2.map(geojson_xy_to_leaflet_xy));
+      const d1 = calculateDistance(leaflet_xy_click, c1);
+      const d2 = calculateDistance(leaflet_xy_click, c2);
+      return d1 - d2;
+    },
+  )[0];
+
+  //if closest correct polgy is a gap, set outerPolygon to the actual parent poly, else iteself.
+  const geojson_xy_outerPolygon =
+    geojson_xy_repairedPolygons.find((repairedPolygon) => {
+      if (!equals(repairedPolygon, geojson_xy_nearestPolygon)) {
+        return booleanContains(
+          polygon([repairedPolygon]),
+          polygon([geojson_xy_nearestPolygon]),
+        );
+      }
+      return false;
+    }) || geojson_xy_nearestPolygon;
+
+  //find all gaps in this outer polygon
+  const geojson_xy_gaps = geojson_xy_repairedPolygons.filter(
+    (geojson_xy_repairedPolygon) =>
+      !equals(geojson_xy_repairedPolygon, geojson_xy_outerPolygon) &&
+      booleanContains(
+        polygon([geojson_xy_outerPolygon]),
+        polygon([geojson_xy_repairedPolygon]),
+      ),
+  );
+
+  const geojson_xy_correctPolygonsWithGapsFilled = [
+    geojson_xy_outerPolygon,
+    ...geojson_xy_gaps,
+  ];
+
+  //check user click is right or wrong:
+  //for checking aginst click, convert everything to our coords
+  const leaflet_xy_outerPolygon = geojson_xy_outerPolygon.map(
+    geojson_xy_to_leaflet_xy,
+  );
+  const ourGaps =
+    geojson_xy_gaps?.map((gap) => gap.map(geojson_xy_to_leaflet_xy)) ?? [];
+  //check if in outer poly
+  const inOuterPoly = booleanPointInPolygon(
+    leaflet_xy_click,
+    polygon([leaflet_xy_outerPolygon]),
+  );
+  // Check if the clicked point is inside any hole
+  const isInsideGap = ourGaps.some((gap) =>
+    booleanPointInPolygon(leaflet_xy_click, polygon([gap])),
+  );
+  //merge the two
+  const correct = inOuterPoly && !isInsideGap;
+
+  const distance = correct
+    ? 0
+    : Math.min(
+        ...correctFeature.geometry.coordinates.map((polygon) =>
+          getDistanceToPolygon(
+            leaflet_xy_click,
+            polygon.map(([x, y]) => geojson_xy_to_leaflet_xy([x, y])),
+          ),
+        ),
+      );
+
+  const leaflet_ll_correctPolygons =
+    geojson_xy_correctPolygonsWithGapsFilled.map((geojson_xy_polygon) =>
+      geojson_xy_polygon
+        .map(([x, y]) => geojson_xy_to_leaflet_xy([x, y]))
+        .map((leaflet_xy) => leaflet_xy_to_leaflet_ll(map, leaflet_xy))
+        .map(({ lat, lng }) => [lng, lat]),
+    );
+
+  return {
+    polygon: {
+      type: 'Feature',
+      geometry: {
+        type: 'Polygon',
+        coordinates: leaflet_ll_correctPolygons,
+      },
+    } as Feature<Polygon>,
+    clicked: correct,
+    distance: distance,
+  };
+};
+
+export const leaflet_ll_to_leaflet_xy = (
+  map: L.Map,
+  leaflet_ll: L.LatLng,
+): Point => {
+  const zoom = map.getMaxZoom();
+  const { x, y } = map.project(leaflet_ll, zoom);
+  return [x, y];
+};
+
+export const leaflet_xy_to_leaflet_ll = (
+  map: L.Map,
+  leaflet_xy: Point,
+): L.LatLng => {
+  const zoom = map.getMaxZoom();
+  const [x, y] = leaflet_xy;
+  return map.unproject(L.point(x, y), zoom);
+};
+
+export const geojson_xy_to_leaflet_xy = ([x, y]: Point): Point =>
+  [x * scaleFactor - 3152, -(y * scaleFactor) + 12400] as Point;
+
+export const leaflet_xy_to_geojson_xy = ([x, y]: Point): Point =>
+  [(x + 3152) / scaleFactor, (y - 12400) / scaleFactor] as Point;
