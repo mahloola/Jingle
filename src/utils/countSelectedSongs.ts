d70b846001710a05405677ced1e7ad41333009fd
mahloola <mahloola08@gmail.com>
2025-07-03 14:48:05 -0400

    countSelectedSongs + sanitizeSongName util added

---
+++ src/utils/countSelectedSongs.ts
@@ -0,0 +1,20 @@
+import { Region, REGIONS, UNDERGROUND_TRACKS } from '../constants/regions';
+import { UserPreferences } from '../types/jingle';
+
+export const countSelectedSongs = (
+  preferences: UserPreferences,
+  region: Region
+) => {
+  const regionSongs = REGIONS[region] || [];
+  const regionUndergroundSongs = UNDERGROUND_TRACKS.filter((song) =>
+    regionSongs.includes(song)
+  );
+  let count = 0;
+  if (preferences.undergroundSelected) {
+    count += regionUndergroundSongs.length;
+  }
+  if (preferences.overgroundSelected) {
+    count += regionSongs.length - regionUndergroundSongs.length;
+  }
+  return count;
+};
