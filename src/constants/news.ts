4e3576661ea69606ede0e908e2a8df9a139037a8
mahloola <mahloola08@gmail.com>
2025-07-04 12:18:17 -0400

    news post

---
+++ src/constants/news.ts
@@ -0,0 +1,39 @@
+export const NEWS_POSTS = [
+  {
+    id: '2',
+    title: `Dungeons!!`,
+    date: 'Jul 3, 2025',
+    content: `
+            <p>
+          150+ dungeon tracks have been added 🎉
+          <br/>
+          (as if the game wasn't hard enough already)
+          <br />- You can disable them in the settings
+          <br />- Some tracks like Inferno don't have an entrance, but exist on the same layer
+        `,
+  },
+  {
+    id: '1',
+    title: `QOL`,
+    date: 'Apr 7, 2025',
+    content: `
+            <p>
+          - Varlamore added
+          <br />- Stats (success%, max streak, etc.)
+          <br />- Preference settings:
+          <br />
+          &emsp;- Region selection
+          <br />
+          &emsp;- 2004 audio
+          <br />
+          &emsp;- Hard mode
+          <br />
+          &emsp;- Confirmation button (no more misclicks)
+          <br />- Misc:
+          <br />
+          &emsp;- Edge distance calculation (vs center) <br />
+          &emsp;- Result copying stats fixed for mobile
+        </p>
+        `,
+  },
+];
